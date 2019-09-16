25ed838ba5453e288c3f3316a1323c699fbf60dd
Frank <frankpei1992@gmail.com>
2019-09-16 16:55:07 +0800

    feat(init): add configure library and init function

---
+++ common/Configure.cmake
@@ -0,0 +1,29 @@
+cmake_minimum_required(VERSION 3.0.2)
+
+# project name forward declaration
+set(CONCRETE_PROJECT_NAME ${PROJECT_NAME} CACHE INTERNAL "Project name")
+
+# function to set project propertys
+# arg0. project root directory, default. ${CMAKE_SOURCE_DIR}
+macro(concrete_init)
+    # set platform compiler target x86 or x64 or arm(not supported yet)
+    if (CMAKE_CL_64)
+        set(CONCRETE_PLATFORM_COMPILER_TARGET x64 CACHE INTERNAL "Platform compiler target")
+    else()
+        set(CONCRETE_PLATFORM_COMPILER_TARGET x86 CACHE INTERNAL "Platform compiler target")
+    endif(CMAKE_CL_64)
+
+    if (ARGV0)
+        message(STATUS "Project root directory will be set as default")
+        set(CONCRETE_PROJECT_SOURCE_DIR ${CMAKE_SOURCE_DIR} CACHE PATH "Project root directory" FORCE)
+    else()
+        set(CONCRETE_PROJECT_SOURCE_DIR ${ARGV0} CACHE PATH "Project root directory" FORCE)
+    endif()
+
+    set(CONCRETE_BINARY_DIR ${CONCRETE_PROJECT_SOURCE_DIR}/${CONCRETE_PLATFORM_COMPILER_TARGET}/bin CACHE PATH "Binary files generate directory" FORCE)
+    set(CONCRETE_LIBRARY_DIR ${CONCRETE_PROJECT_SOURCE_DIR}/${CONCRETE_PLATFORM_COMPILER_TARGET}/lib CACHE PATH "Library files generate directory" FORCE)
+
+    set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CONCRETE_LIBRARY_DIR})
+    set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CONCRETE_LIBRARY_DIR})
+    set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CONCRETE_BINARY_DIR})    
+endmacro()
