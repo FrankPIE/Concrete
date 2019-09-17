60226e06f9d66a9b87a5ce73cf7f8b28c9ce3f08
Frank <frankpei1992@gmail.com>
2019-09-17 20:34:35 +0800

    style: file Opeartor Change Params Count

--- common/FileOperator.cmake
+++ common/FileOperator.cmake
@@ -1,8 +1,17 @@
 cmake_minimum_required(VERSION 3.0.2)
 
 # file download function
-function(concrete_file_download uri target_relative_path)
-    set(_target ${CONCRETE_PROJECT_SOURCE_DIR}/${target_relative_path})
+# ARGV0 uri
+# ARGV1 relative_path
+# ARGV2 hash algorithm
+# ARGV3 hash hex string 
+function(concrete_file_download)
+    # params check
+    if (NOT ARGV0 OR NOT ARGV1)
+        message(FATAL_ERROR "bad call: few params, shoud pass url and relative_path")
+    endif()
+
+    set(_target ${CONCRETE_PROJECT_SOURCE_DIR}/${ARGV1})
 
     # check file
     if (EXISTS ${_target})
@@ -18,16 +27,16 @@ function(concrete_file_download uri target_relative_path)
 
     # download file
     if (NOT EXISTS ${_target})
-        message(STATUS "DOWNLOADING ${uri}...")
+        message(STATUS "DOWNLOADING ${ARGV0}...")
 
-        file(DOWNLOAD ${uri} ${_target} SHOW_PROGRESS STATUS ERR)
+        file(DOWNLOAD ${ARGV0} ${_target} SHOW_PROGRESS STATUS ERR)
 
         list(GET ERR 0 ERR_CODE)
         
         if (ERR_CODE)
             file(REMOVE ${_target})
             list(GET ERR 1 ERR_MESSAGE)
-            message(FATAL_ERROR "Failed to download file ${uri}: ${ERR_MESSAGE}")
+            message(FATAL_ERROR "Failed to download file ${ARGV0}: ${ERR_MESSAGE}")
         endif ()
 
         # check
@@ -36,7 +45,7 @@ function(concrete_file_download uri target_relative_path)
 
             if (NOT "${_hash_value}" STREQUAL "${ARGV3}")
                 file(REMOVE ${_target})
-                message(FATAL_ERROR "Failed to download file ${uri}: file has been broken")
+                message(FATAL_ERROR "Failed to download file ${ARGV0}: file has been broken")
             endif()
         endif()
     endif()
