b6b7f499409518c3fbb92cc22d789d20d1419c40
Frank <frankpei1992@gmail.com>
2019-12-04 22:46:11 +0800

    feat: add new module StringHelper and add function CONCRETE_METHOD_STRING_POP_LAST

---
+++ modules/Unitity/ConcreteStringHelper.cmake
@@ -0,0 +1,37 @@
+# MIT License
+# 
+# Copyright (c) 2019 MadStrawberry
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+INCLUDE_GUARD(GLOBAL)
+
+MACRO(CONCRETE_METHOD_STRING_POP_LAST VALUE POP_COUNT OUTPUT)
+    SET(length)
+    STRING(LENGTH "${${VALUE}}" length)
+    MATH(EXPR length "${length} - ${POP_COUNT}")
+
+    IF(${length} LESS 0)
+        MESSAGE(FATAL_ERROR "Pop too many char")
+    ELSE()
+        STRING(SUBSTRING "${${VALUE}}" 0 ${length} ${OUTPUT})
+    ENDIF(${length} LESS 0)
+
+    UNSET(length)
+ENDMACRO(CONCRETE_METHOD_STRING_POP_LAST)
+
